ea064b26eb58c1f31579c8e33d1732d6d8a3773e
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-06 08:38:01 +0330

    Add project files.

---
+++ WizLib.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WizLib.Models.Models;
+
+namespace WizLib.DataAccess.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Category> Categories { get; set; }
+	//public DbSet<Genre> Genres { get; set; }
+}
---
+++ WizLib.Models/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizLib.Models.Models;
+
+public class Genre
+{
+    public int GenreId { get; set; }
+    public string GenreName { get; set; }
+}
