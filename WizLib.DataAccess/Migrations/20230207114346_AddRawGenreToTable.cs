eaf00e0bd563c77733551dcb4cf4b3a53cf55ef9
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-08 19:33:57 +0330

    AddRawGenreToTheTable

---
+++ WizLib.DataAccess/Migrations/20230207114346_AddRawGenreToTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WizLib.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRawGenreToTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into tbl_genres values ('Gen 1')");
+            migrationBuilder.Sql("insert into tbl_genres values ('Gen 2')");
+            migrationBuilder.Sql("insert into tbl_genres values ('Gen 3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WizLib/Controllers/GernreController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WizLib.DataAccess.Data;
+using WizLib.Models.Models;
+
+namespace WizLib.Controllers;
+
+public class GernreController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public GernreController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Genre> objList = _db.Genres.ToList();
+        return View(objList);
+    }
+}
